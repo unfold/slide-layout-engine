6ac38e0c5f249ee93170f3555f8313839fffd215
Daniel <daniel@unfold.no>
2011-12-02 14:53:49 +0100

    Layout working, with scroll. Tested in all browsers

--- js/main.js
+++ js/main.js
@@ -1,57 +1,91 @@
 (function($) {
-    var initLayout = function($sections, options) {
-        var $previous, $current;
+    var initLayout = function($container, options) {
+        var $current,
+            $previous,
+            params;
+
+        $sections = $container.find('.section');
 
         var settings = {
             animate: true,
             direction: 'left',
             easing: 'linear',
-            duration: 800,
+            duration: 200,
             offset: 0
         };
 
         $.extend(settings, options);
 
-        var setSection = function($section, options) {
-            // Return if section is same
-            if($sections.filter('.current').get(0) === $section.get(0)) { return; }
+        var updateViewportLayout = function() {
+            // Scroll container height
+            var content = $current.find('.content');
+            var height = Math.max($(window).height(), content.outerHeight());
+            $container[params.animate ? 'animate' : 'css']({ height: height }, params.duration, params.easing);
 
-            var s = $.extend({}, settings, options);
+            // Section height to window height
+            $sections.css({
+                width: $container.width(),
+                height: $(window).height()
+            });
+        };
 
-            var dir = s.direction;
+        $sections.scroll(function(e) {
+            e.preventDefault();
+        });
+
+        $(window).resize(function() {
+            params.animate = false;
+            updateViewportLayout();
+        });
+
+        var updateSectionLayout = function() {
+            var p = params;
+
+            var dir = p.direction;
             var dirHorizontal = dir === 'left' || dir === 'right';
-
-            var back = $previous && $previous.get(0) === $section.get(0);
-
-            $previous = $sections.filter('.current').addClass('previous').removeClass('current');
-            $current = $section.addClass('current');
 
             var css = {
                 prev: {
                     from: {},
                     to: {
-                        left: dirHorizontal ? (dir === 'left'   ? (-100+s.offset)+'%' : (100-s.offset)+'%') : '0%',
-                        top: !dirHorizontal ? (dir === 'up'     ? (-100+s.offset)+'%' : (100-s.offset)+'%') : '0%'
+                        left: dirHorizontal ? (dir === 'left'   ? (-100+p.offset)+'%' : (100-p.offset)+'%') : '0%',
+                        top: !dirHorizontal ? (dir === 'up'     ? (-100+p.offset)+'%' : (100-p.offset)+'%') : '0%'
                     }
                 },
                 next: {
-                    from: back ? {} : {
+                    from: p.back ? {} : {
                         left: dirHorizontal ? (dir === 'left'   ? '100%' : '-100%') : '0%',
                         top: !dirHorizontal ? (dir === 'up'     ? '100%' : '-100%') : '0%'
                     },
                     to: {
-                        left: dirHorizontal ? (dir === 'left'   ? s.offset+'%' : -s.offset+'%') : '0%',
-                        top: !dirHorizontal ? (dir === 'up'     ? s.offset+'%' : -s.offset+'%') : '0%'
+                        left: dirHorizontal ? (dir === 'left'   ? p.offset+'%' : -p.offset+'%') : '0%',
+                        top: !dirHorizontal ? (dir === 'up'     ? p.offset+'%' : -p.offset+'%') : '0%'
                     }
                 }
             };
 
-            $previous.stop().css(css.prev.from)[s.animate ? 'animate' : 'css'](css.prev.to, s.duration, s.easing);
-            $current.stop().css(css.next.from)[s.animate ? 'animate' : 'css'](css.next.to, s.duration, s.easing);
+            $previous.stop().css(css.prev.from)[p.animate ? 'animate' : 'css'](css.prev.to, p.duration, p.easing);
+            $current.stop().css(css.next.from)[p.animate ? 'animate' : 'css'](css.next.to, p.duration, p.easing);
+
+            updateViewportLayout();
+        };
+
+        var setSection = function($section, options) {
+            if($sections.filter('.current').get(0) === $section.get(0)) { return; }
+
+            params = $.extend({}, settings, options);
+            params.back = $previous && $previous.get(0) === $section.get(0);
+
+            $previous = $sections.filter('.current').addClass('previous').removeClass('current').trigger('deactivate');
+            $current = $section.addClass('current').trigger('activate');
+
+            updateSectionLayout();
         };
 
         var setOffset = function(offset) {
-
+            params.offset = offset;
+            params.animate = false;
+            updateSectionLayout();
         };
 
         return {
@@ -60,21 +94,40 @@
         };
     };
 
-    $(function() {
-        var layout = initLayout($('.section'));
 
-        layout.setSection($('.home.section'), { animate: false });
+
+
+    $(function() {
+        var layout = initLayout($('.scroll-container'));
+
+        var home = $('.home.section');
+        var scores = $('.scores.section');
+
+        var getScoreOffset = function() {
+            var homeMargin = ($(window).width() - 980) * 0.5 + 200;
+            var homeMarginPercent = (homeMargin / $(window).width()) * 100;
+            var offset = homeMarginPercent;
+            return parseFloat(offset.toFixed(2));
+        };
+
+        scores.on('activate', function() {
+            $(window).on('resize.scoreOffset', function() {
+                layout.setOffset(getScoreOffset());
+            });
+        });
+
+        scores.on('deactivate', function() {
+            $(window).off('resize.scoreOffset');
+        });
 
         $('.home.section').click(function() {
-           layout.setSection($('.scores.section'), { offset: 20 });
+           layout.setSection($('.scores.section'), { offset: getScoreOffset() });
         });
 
         $('.scores.section').click(function() {
            layout.setSection($('.home.section'), { direction: 'right' });
         });
 
-        $(document).keyup(function() {
-            layout.setOffset(40);
-        });
+        layout.setSection($('.home.section'), { animate: false });
     });
 })(jQuery);
